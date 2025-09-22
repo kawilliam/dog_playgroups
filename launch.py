4d43295313f523c72f7da53fc95beee3d7ed0e9d
Kyle Williamson <mgkshome7@gmail.com>
2025-09-21 21:34:22 -0400

    Adjust packaging setup

--- launch.py
+++ launch.py
@@ -5,9 +5,14 @@ BASE = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
 APP = os.path.join(BASE, "app.py")
 
 os.environ.setdefault("STREAMLIT_BROWSER_GATHER_USAGE_STATS", "false")
-os.environ.setdefault("STREAMLIT_SERVER_PORT", "0")
+os.environ["STREAMLIT_GLOBAL_DEVELOPMENTMODE"] = "false"
 
 from streamlit.web.cli import main as st_main
 
-sys.argv = ["streamlit", "run", APP, "--server.headless=false"]
+sys.argv = [
+    "streamlit", "run", APP,
+    "--global.developmentMode=false",
+    "--server.headless=false",
+    "--server.port=0",
+]
 st_main()
